88a11ce83ec70ea8b9f4abb2130504613a42f32b
Gabriel Honda <gabrielhondacba@gmail.com>
2024-06-08 18:04:06 -0400

    chore: commit inicial 🎉

---
+++ src/main/java/dev/da0hn/keycloak/course/KeycloakCourseApplication.java
@@ -0,0 +1,13 @@
+package dev.da0hn.keycloak.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakCourseApplication.class, args);
+	}
+
+}
