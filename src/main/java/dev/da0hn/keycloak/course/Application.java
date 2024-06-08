ea33c6fa5b8fdcd29111e4815a93e1f7d4e75ee8
Gabriel Honda <gabrielhondacba@gmail.com>
2024-06-08 18:06:36 -0400

    refactor: renomeia para `Application`

--- src/main/java/dev/da0hn/keycloak/course/Application.java
+++ src/main/java/dev/da0hn/keycloak/course/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KeycloakCourseApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KeycloakCourseApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
